db76ac5aec62fc63df894cce13b79609da0460f7
matheusrb <matheusrb@matheus.myguest.virtualbox.org>
2025-04-23 08:13:20 -0300

    APS 3 DONE

--- amr_ws/src/rm_localization/setup.py
+++ amr_ws/src/rm_localization/setup.py
@@ -14,6 +14,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'map'), glob('map/*.yaml')),
+        (os.path.join('share', package_name, 'map'), glob('map/*.pgm')),
 
     ],
     install_requires=['setuptools'],
