413cbfdfb79f21aed2d0ed97993ff736b314e86b
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-05-22 19:17:50 -0300

    Revert "added new robot Jackal"
    This reverts commit ae2bd7124ed3e30877c38ae98f08246914ca253e.

--- final_project_ws/install/custom_interfaces/share/custom_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(custom_interfaces_IDL_FILES "msg/Aula7.idl;srv/Aula8.idl;action/Aula9.idl;action/Rotate.idl")
-set(custom_interfaces_INTERFACE_FILES "msg/Aula7.msg;srv/Aula8.srv;srv/Aula8_Request.msg;srv/Aula8_Response.msg;action/Aula9.action;action/Rotate.action")
